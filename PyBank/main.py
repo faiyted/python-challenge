ac08d813d335dacd06c6c4662a7a1b672eebde54
faiyted <faiyted@gmail.com>
2024-07-17 14:49:33 -0500

    Adding export to txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,7 @@ import csv
 budget_csv = 'C:/Users/Module 3/Starter_Code/PyBank/Resources/budget_data.csv'
 # Path to collect data from the Resources folder
 # budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
-
+output_file = "analysis.txt"
 if not os.path.exists(budget_csv):
     print("File does not exist.")
 else:
@@ -21,6 +21,10 @@ else:
         amount = []
         months = []
         changes = []
+        results = []
+        results.append("Financial Analysis")
+        results.append("-------------------------")
+        
         # Loop through the data- Total Months/ Total Amount/ 
         for row in csvreader:
             amount.append(int(row[1]))
@@ -28,6 +32,10 @@ else:
 
             total_months = len(set(months))
             total_amt = sum(amount)
+        results.append(f"Total Months:{ total_months}")
+        results.append(f"Total: ${total_amt}") 
+        
+        
         # Loop through the data- Fidning Average Change/ Greatest Increase/ Greatest Decrease
         for i in range(1, len(amount)):
             changes.append(amount[i] - amount[i-1])
@@ -47,11 +55,21 @@ else:
                 minDecrease= 0
                 maxMon = ""
                 minMon = ""
-        # Print the results
-        print("Financial Analysis")
-        print("-------------------------")
-        print("Total Months:", total_months)
-        print("Total: $" + str(sum(amount)))
-        print("Average Change: $" + str(round(avg, 2)))
-        print(f"Greatest Increase in Profits: {maxMon} (${maxIncrease})")
-        print(f"Greatest Decrease in Profits: {minMon} (${minDecrease})")
+        
+        results.append(f"Average Change: ${round(avg, 2)}")
+        results.append(f"Greatest Increase in Profits: {maxMon} (${maxIncrease})")
+        results.append(f"Greatest Decrease in Profits: {minMon} (${minDecrease})")
+                
+                
+        # Print results to console
+        
+             
+        for line in results:
+                print(line)
+
+        # Export results to a text file
+
+        with open(output_file, "w") as outfile:
+                for line in results:
+                    outfile.write(line + "\n")
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,7 @@ import csv
 # Path to collect data from the Resources folder
 # vote_csv = os.path.join('..', 'Resources', 'election_data.csv')
 vote_csv = r"C:\Users\Module 3\Starter_Code\PyPoll\Resources\election_data.csv"
+output_file = "analysis.txt"
 if not os.path.exists(vote_csv):
     print("File does not exist.")
 else:
@@ -16,6 +17,7 @@ else:
         votes = {}
         total_votes = 0
         persons = []
+        results = []
         # Loop through the data - total votes
         for row in csvreader:
             total_votes += 1
@@ -25,16 +27,25 @@ else:
                 votes[person] +=1
             else:
                 votes[person] = 1
-    print("Election Results")
-    print("-------------------------")
-    print(f"Total Votes: {total_votes}")
-    print("-------------------------")
+    results.append("Election Results")
+    results.append("-------------------------")
+    results.append(f"Total Votes: {total_votes}")
+    results.append("-------------------------")
     # Loop through the data - Each candidate’s total votes and percent of votes
     for person, vote_count in votes.items():
         percentage = (vote_count / total_votes) * 100
-        print(f"{person}: {percentage:.3f}% ({vote_count})")
+        results.append(f"{person}: {percentage:.3f}% ({vote_count})")
     # using Mac function finding the winner
     winner = max(votes, key=votes.get)
-    print("-------------------------")
-    print(f"Winner: {winner}")
-    print("-------------------------")
+    results.append("-------------------------")
+    results.append(f"Winner: {winner}")
+    results.append("-------------------------")
+
+    # Print results to console
+    for line in results:
+        print(line)
+
+    # Export results to a text file
+    with open(output_file, "w") as outfile:
+                for line in results:
+                    outfile.write(line + "\n")
